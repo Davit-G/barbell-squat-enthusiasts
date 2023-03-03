f769df756565eec2ca2aefd758eb43eec3b8a01a
dave <dgog0001@student.monash.edu>
2023-03-04 05:25:26 +1100

    set up CORS middleware, basic mongo connection information

--- unihack2023-server/main.py
+++ unihack2023-server/main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
 
+# import CORS middleware 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# mongodb database connecton
+import pymongo
+from pymongo import MongoClient
+
+# connect to mongodb
+client = MongoClient("mongodb://localhost:27017/") # change later to string from api_key file thing idk
+db = client["unihack2023"] # the database name
+
+# create a collection
+collection = db["users"]
 
 @app.get("/")
 async def root():
