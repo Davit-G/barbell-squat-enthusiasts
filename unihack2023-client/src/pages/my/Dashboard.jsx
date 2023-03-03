e934c87d9d77cde8246959f6990e586c82807594
Dawaad <jntucker02@gmail.com>
2023-03-04 10:33:12 +1100

    Fixed Classname Error

--- unihack2023-client/src/pages/my/Dashboard.jsx
+++ unihack2023-client/src/pages/my/Dashboard.jsx
@@ -23,7 +23,7 @@ function Dashboard({ }) {
                 <div className='rounded-xl shadow-lg w-60 md:w-100 p-4 px-8 h-[50vh] flex items-start flex-col justify-start'>  {/* Navigation on the left */}
                     <div className='flex flex-col justify-center items-start'>
                         <h1 className='text-2xl font-semibold text-left'>Welcome, User</h1>
-                        <div className='mt-4 flex flex-col space-y-3>
+                        <div className='mt-4 flex flex-col space-y-3'>
                             <Link to="/my/" className='text-base md:text-xl text-left hover:underline text-blue-500'>Project Overview</Link>
                             <Link to="/my/tasks" className='text-base md:text-xl text-left hover:underline text-blue-500'>Today's Tasks</Link>
                             <Link to="/my/calendar" className='text-base md:text-xl text-left hover:underline text-blue-500'>Calendar View</Link>
