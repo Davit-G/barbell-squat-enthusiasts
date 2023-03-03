48a78a5c2995b4669a44409730dd83a43d88703b
dave <dgog0001@student.monash.edu>
2023-03-04 03:08:43 +1100

    chore: configure image compression plugin for vite

--- unihack2023-client/vite.config.js
+++ unihack2023-client/vite.config.js
@@ -1,9 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import WindiCSS from 'vite-plugin-windicss'
+import viteImagemin from 'vite-plugin-imagemin' // image compression plugin, automatic dont worry
+
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), WindiCSS()],
+    plugins: [
+        react(),
+        WindiCSS(),
+        viteImagemin({
+            gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false,
+            },
+            optipng: {
+                optimizationLevel: 7,
+            },
+            mozjpeg: {
+                quality: 20,
+            },
+            pngquant: {
+                quality: [0.8, 0.9],
+                speed: 4,
+            },
+            svgo: {
+                plugins: [
+                    {
+                        name: 'removeViewBox',
+                    },
+                    {
+                        name: 'removeEmptyAttrs',
+                        active: false,
+                    },
+                ],
+            },
+        }),
+    ],
 })
