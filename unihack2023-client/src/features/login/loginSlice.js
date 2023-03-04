956217780bfabbf54c65ae5a53f2470b500a7fa5
Dawaad <jntucker02@gmail.com>
2023-03-04 15:03:59 +1100

    Implemented Firebase Login, Redux store login details

--- unihack2023-client/src/features/login/loginSlice.js
+++ unihack2023-client/src/features/login/loginSlice.js
@@ -1,23 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { // default state when the app starts up
-    loggedIn: true,
-}
+const initialState = {
+  // default state when the app starts up
+  loggedIn: true,
+  
+  accessToken: "",
+  uid: "",
+  displayName: "",
+};
 
 export const loginSlice = createSlice({
-    name: 'login', // make sure this name is unique but also the same as the reducer name in store.js
-    initialState,
-    reducers: {
-        setLogin: (state, action) => {
-            state.loggedIn = action.payload
-        },
+  name: "login", // make sure this name is unique but also the same as the reducer name in store.js
+  initialState,
+  reducers: {
+    setLogin: (state, action) => {
+      state.loggedIn = action.payload;
     },
-})
+    setUserDetails: (state, action) => {
+      state.displayName = action.payload.displayName;
+      state.accessToken = action.payload.accessToken;
+      state.uid = action.payload.uid;
+    },
+  },
+});
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = loginSlice.actions
+export const { setLogin, setUserDetails } = loginSlice.actions;
 
 // export the selector
-export const selectLogin = state => state.login.loggedIn
-
-export default loginSlice.reducer
+export const selectLogin = (state) => state.login.loggedIn;
+export const selectAccessToken = (state) => state.login.accessToken;
+export const selectDispayName = (state) => state.login.displayName;
+// export const selectUserDetails = (state) => state.login.setUserDetails;
+export default loginSlice.reducer;
--- unihack2023-client/src/main.jsx
+++ unihack2023-client/src/main.jsx
@@ -1,68 +1,77 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 
-import '../styles/main.css'
+import "../styles/main.css";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom"; // configuring the browser routing things
 import Layout from "./Layout";
 import Calendar from "./pages/my/Calendar";
 import Week from "./pages/my/Week";
 import Error from "./pages/ErrorPage";
 
-import { store } from './app/store'
-import { Provider } from 'react-redux'
-
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { initializeApp } from "firebase/app";
 
 // each route is loaded asynchronously
 const Home = React.lazy(() => import("./pages/HomePage.jsx"));
 const Login = React.lazy(() => import("./pages/Login.jsx"));
 const Tasks = React.lazy(() => import("./pages/my/Tasks.jsx"));
 const Dashboard = React.lazy(() => import("./pages/my/Dashboard.jsx"));
-const ErrorPage = React.lazy(() => import("./pages/ErrorPage.jsx"))
+const ErrorPage = React.lazy(() => import("./pages/ErrorPage.jsx"));
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDh3icl4HE5zlXzkd5c2awSv6mSN26EveM",
+    authDomain: "unihack2023-9ba75.firebaseapp.com",
+    projectId: "unihack2023-9ba75",
+    storageBucket: "unihack2023-9ba75.appspot.com",
+    messagingSenderId: "72795354157",
+    appId: "1:72795354157:web:3b4982750ba7f3460274a4"
+  };
+
+const app = initializeApp(firebaseConfig);
 
 const routes = (
-    <>
-        <Route path="/" element={<Home />} />
+  <>
+    <Route path="/" element={<Home />} />
 
-        {/* Add more routes here, make sure they are ASYNC */}
-        <Route path="login" element={<Login />} />
-        <Route path="tasks" element={<Tasks />} />
-        <Route path="my" element={<Dashboard />}>
-            {" "}
-            {/* this is a nested route */}
-            <Route path="tasks" element={<Tasks />} />
-            <Route path="calendar" element={<Calendar />} />
-            <Route path="week" element={<Week />} />
-
-        </Route>
-        <Route path="*" element={<ErrorPage />} />
-    </>
+    {/* Add more routes here, make sure they are ASYNC */}
+    <Route path="login" element={<Login />} />
+    <Route path="tasks" element={<Tasks />} />
+    <Route path="my" element={<Dashboard />}>
+      {" "}
+      {/* this is a nested route */}
+      <Route path="tasks" element={<Tasks />} />
+      <Route path="calendar" element={<Calendar />} />
+      <Route path="week" element={<Week />} />
+    </Route>
+    <Route path="*" element={<ErrorPage />} />
+  </>
 );
 
 function Fallback() {
-    // a temporary page to display while the current page is loading
+  // a temporary page to display while the current page is loading
 
-    return <div className="mx-auto my-auto text-center">Loading...</div>;
+  return <div className="mx-auto my-auto text-center">Loading...</div>;
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-
-        <Provider store={store}> {/* handles redux */}
-
-            <BrowserRouter> {/* handles routing */}
-                <Layout> {/* handles the layout of the page */}
-
-                    <React.Suspense fallback={<Fallback />}> {/* if page not loaded yet, display temporary page thing */}
-
-                        <Routes>
-                            {routes}
-                        </Routes>
-
-                    </React.Suspense>
-                </Layout>
-            </BrowserRouter>
-
-        </Provider>
-
-    </React.StrictMode>,
-)
+  <React.StrictMode>
+    <Provider store={store}>
+      {" "}
+      {/* handles redux */}
+      <BrowserRouter>
+        {" "}
+        {/* handles routing */}
+        <Layout>
+          {" "}
+          {/* handles the layout of the page */}
+          <React.Suspense fallback={<Fallback />}>
+            {" "}
+            {/* if page not loaded yet, display temporary page thing */}
+            <Routes>{routes}</Routes>
+          </React.Suspense>
+        </Layout>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
