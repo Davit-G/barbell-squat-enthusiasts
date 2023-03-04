5d738606ff675fd4a91f8018e7bbb1ebe9add10d
dave <razormayhem4z@gmail.com>
2023-03-05 06:35:43 +1100

    resolved issue with pipe symbol for optional type

--- unihack2023-server/schemas/task.py
+++ unihack2023-server/schemas/task.py
@@ -1,8 +1,11 @@
 from pydantic import BaseModel
 
+# import optional
+from typing import Optional
+
 class Task(BaseModel):
     task_id: str
     title: str
     start_date: str
     end_date: str
-    description: str | None = None
+    description: Optional[str] = None
