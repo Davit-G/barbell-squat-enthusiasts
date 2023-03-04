263d219f38e0dee5e574cb59aebc173c08db08f6
Dawaad <jntucker02@gmail.com>
2023-03-04 13:23:52 +1100

    Implementation of Day selector

--- unihack2023-client/src/pages/my/Week.jsx
+++ unihack2023-client/src/pages/my/Week.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { startOfToday } from 'date-fns'
 function Week() {
+    const today = startOfToday()
+    const [selectedDay, setSelectedDay] = useState(today)
+
+    const dayToText = (day) => {
+        switch (day) {
+          case 0:
+            return "Sunday";
+          case 1:
+            return "Monday";
+          case 2:
+            return "Tuesday";
+          case 3:
+            return "Wednesday";
+          case 4:
+            return "Thursday";
+          case 5:
+            return "Friday";
+          case 6:
+            return "Saturday";
+        }   
+      }
   return (
-    <div className='m-4'>
+    <div className='p-4 md:pl-20'>
     
             <h1 className='text-4xl font-semibold text-left'>Here is what your week looks like:</h1>
+            <div>
+                {selectedDay.getDay()}
+
+            </div>
             <div className='flex flex-col justify-start items-start mt-4 w-full'> {/* Tasks go here */}
                 {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((task) => {
                     return (
