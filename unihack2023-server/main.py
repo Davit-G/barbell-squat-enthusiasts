bd933ab0ee7cfdde2cde0690788972ef0d328da3
cros107 <ckaramanos@me.com>
2023-03-04 11:26:35 +1100

    a

--- unihack2023-server/main.py
+++ unihack2023-server/main.py
@@ -1,8 +1,49 @@
 from fastapi import FastAPI
-
+import requests
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+url = "https://api.openai.com/v1/engines/davinci-codex/completions"
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "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"
+}
+
+data = {
+    "prompt": "Hello,"
+}
+
+def build_prompt(data):
+    data += """
+    Give me a list of subtasks that I can do to work towards this goal.
+    Return tasks in the json format below.
+
+        ```json
+        {
+            "subtasks": [subtask]
+        }```
+
+        where each subtask in "subtasks" is a json object with the format
+        ```json
+        {
+            "name": "",
+            "description": ""
+        }```
+
+        Please give me less than 10 subtasks.
+        Please explicitly order the tasks.
+    """
+    return data
+
+@app.post("/submit")
+async def submit(data):
+    #assuming data is just string containing data?
+    data = build_prompt(data)
+    response = requests.post(url, headers=headers, json={"prompt" : data})
+
+    print(response)
+
+    return data
+
