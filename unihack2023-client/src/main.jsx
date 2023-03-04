173482ed523c5a84e01d17d26af5cac0960eb6b7
dave <dgog0001@student.monash.edu>
2023-03-04 13:33:48 +1100

    fix issues with merge

--- unihack2023-client/src/main.jsx
+++ unihack2023-client/src/main.jsx
@@ -20,47 +20,32 @@ const Dashboard = React.lazy(() => import("./pages/my/Dashboard.jsx"));
 const ErrorPage = React.lazy(() => import("./pages/ErrorPage.jsx"))
 
 const routes = (
-  <>
-    <Route path="/" element={<Home />} />
+    <>
+        <Route path="/" element={<Home />} />
 
-    {/* Add more routes here, make sure they are ASYNC */}
-    <Route path="login" element={<Login />} />
-    <Route path="tasks" element={<Tasks />} />
-    <Route path="my" element={<Dashboard />}>
-      {" "}
-      {/* this is a nested route */}
-      <Route path="tasks" element={<Tasks />} />
-      <Route path="calendar" element={<Calendar />} />
-      <Route path="week" element={<Week/>}/>
+        {/* Add more routes here, make sure they are ASYNC */}
+        <Route path="login" element={<Login />} />
+        <Route path="tasks" element={<Tasks />} />
+        <Route path="my" element={<Dashboard />}>
+            {" "}
+            {/* this is a nested route */}
+            <Route path="tasks" element={<Tasks />} />
+            <Route path="calendar" element={<Calendar />} />
+            <Route path="week" element={<Week />} />
 
-    </Route>
-    <Route path="*" element={<ErrorPage />} />
-  </>
+        </Route>
+        <Route path="*" element={<ErrorPage />} />
+    </>
 );
 
 function Fallback() {
-  // a temporary page to display while the current page is loading
+    // a temporary page to display while the current page is loading
 
-  return <div className="mx-auto my-auto text-center">Loading...</div>;
+    return <div className="mx-auto my-auto text-center">Loading...</div>;
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      {" "}
-      {/* handles routing */}
-      <Layout>
-        {" "}
-        {/* handles the layout of the page */}
-        <React.Suspense fallback={<Fallback />}>
-          {" "}
-          {/* if page not loaded yet, display temporary page thing */}
-          <Routes>{routes}</Routes>
-        </React.Suspense>
-      </Layout>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+    <React.StrictMode>
 
         <Provider store={store}> {/* handles redux */}
 
