062054c54f9616abc0eb186848ed560d0bdb94c3
Yao <86668901+YueYaoYan@users.noreply.github.com>
2023-03-04 12:33:43 +1100

    Feat: Added new update and delete

--- unihack2023-server/calendarController.py
+++ unihack2023-server/calendarController.py
@@ -11,11 +11,11 @@ from googleapiclient.errors import HttpError
 
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-def getEvent():
+def getEvent(title="",loc="",desc="",start_date="",end_date=""):
     return {
-    'summary': 'Google I/O 2015',
-    'location': '800 Howard St., San Francisco, CA 94103',
-    'description': 'A chance to hear more about Google\'s developer products.',
+    'summary': 'Google I/O 2015', # title
+    'location': '800 Howard St., San Francisco, CA 94103', 
+    'description': 'A chance to hear more about Google\'s developer products.', 
     'start': {
         'dateTime': '2015-05-28T09:00:00-07:00',
         'timeZone': 'America/Los_Angeles',
@@ -32,7 +32,7 @@ def getEvent():
         {'email': 'sbrin@example.com'},
     ],
     'reminders': {
-        'useDefault': False,
+        'useDefault': False, # set to True if no reminder
         'overrides': [
         {'method': 'email', 'minutes': 24 * 60},
         {'method': 'popup', 'minutes': 10},
@@ -40,16 +40,17 @@ def getEvent():
     },
     }
 
-def getEvents():
+def getEvents(maxResults=10):
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
     # Call the Calendar API
     service = checkAuth()
+    
     now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
     print('Getting the upcoming 10 events')
     events_result = service.events().list(calendarId='primary', timeMin=now,
-                                            maxResults=10, singleEvents=True,
+                                            maxResults=maxResults, singleEvents=True,
                                             orderBy='startTime').execute()
     events = events_result.get('items', [])
 
@@ -95,12 +96,38 @@ def createEvent(eventJson):
         eventJson (string): json string of event
 
     Returns:
-        string: json string of created event
+        dictionary: dictionary for created event
     """
     service = checkAuth()
     
     event = service.events().insert(calendarId='primary', body=eventJson).execute()
     return event
+
+
+def deleteEvent(eventId):
+    """Delete existing event for user.
+
+    Args:
+        calendarId (string): calendar id of event
+    """
+    service = checkAuth()
+    
+    res = service.events().delete(calendarId='primary', eventId=eventId).execute()
+    return res
+
+
+def modifyEvent(event):
+    """Delete existing event for user.
+
+    Args:
+        calendarId (string): calendar id of event
+    """
+    service = checkAuth()
+
+    updated_event = service.events().update(calendarId='primary', eventId=event['id'], body=event).execute()
+
+    # Print the updated date.
+    print (updated_event['updated'])
 
 
 if __name__ == '__main__':
